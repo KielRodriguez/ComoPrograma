ec8b6640d9974755c72a1b76a973242aa9182501
KielRodriguez <kr.rdz.20@gmail.com>
2015-10-13 00:30:42 -0500

    commit inicial

---
+++ EstudioGUI/DibujarCaraSonriente.java
@@ -0,0 +1,28 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+public class DibujarCaraSonriente extends JPanel{
+  public void paintComponent(Graphics g){
+    super.paintComponent(g);
+
+    //Dibuja la cara
+    g.setColor(Color.YELLOW);
+    g.fillOval(10,10,200,200);
+
+    //Dibuja los ojos
+    g.setColor(Color.BLACK);
+    g.fillOval(55, 65, 30, 30);
+    g.fillOval(132, 65, 30, 30);
+
+    //Dibuja boca
+    g.fillOval(50, 110, 120, 60);
+
+     //Convierte la boca en sonrisa
+     g.setColor(Color.YELLOW);
+     g.fillRect(50, 110, 120, 30);
+     g.fillOval(50, 120, 120, 40);
+
+  }
+
+}
